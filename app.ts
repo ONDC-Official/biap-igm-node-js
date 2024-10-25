b3d2d14d94d42f142736e2c717cc20532f044265
Ubuntu <ubuntu@ip-172-31-35-209.ap-south-1.compute.internal>
2024-10-25 10:30:17 +0000

    chore: remove firebase use

--- app.ts
+++ app.ts
@@ -5,14 +5,14 @@ import loadEnvVariables from "./utils/envHelper";
 import issueRoutes from "./routes/issue";
 import issue_statusRoutes from "./routes/issue_status";
 import sseRoutes from "./routes/sse";
-import initializeFirebase from "./lib/firebase/initializeFirebase";
+// import initializeFirebase from "./lib/firebase/initializeFirebase";
 
 const createServer = (): express.Application => {
   const app: Application = express();
 
   // initialize environment variables
   loadEnvVariables();
-  initializeFirebase();
+  // initializeFirebase();
   // Body parsing Middleware
   app.use(express.json({ limit: "50mb" }));
   app.use(bodyParser.json());
