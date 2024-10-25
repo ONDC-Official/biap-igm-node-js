c62d4cf17799ccc5339819f0f9ff5c7b021c8d08
NishthaMonga <57255861+NishthaMonga@users.noreply.github.com>
2024-10-25 16:55:18 +0530

    chore: json remove

--- lib/firebase/initializeFirebase.ts
+++ lib/firebase/initializeFirebase.ts
@@ -1,11 +1,11 @@
 import admin from "firebase-admin";
-import firebaseService from "../../config/dev/firebase-service-account.json";
+// import firebaseService from "../../config/dev/firebase-service-account.json";
 
-const firebaseAdminObject: any = firebaseService;
+// const firebaseAdminObject: any = firebaseService;
 
 const initializeFirebase = () => {
   admin.initializeApp({
-    credential: admin.credential.cert(firebaseAdminObject),
+    // credential: admin.credential.cert(firebaseAdminObject),
   });
 };
 
